14234540b7e93dc791bd5b2f2d2033af81d797a7
Hossam <hossamegyqandel@gmail.com>
2023-01-08 19:57:32 +0200

    feat(Person): Add new person functionality
    Implemented the ability to get all users from Database using GET request method

--- src/main/java/com/google/application/dao/PersonDao.java
+++ src/main/java/com/google/application/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.google.application.dao;
 
 import com.google.application.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -12,4 +13,6 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }
